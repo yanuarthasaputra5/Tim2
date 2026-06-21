a2cf84f9c65dd2628ad11b49b2ff914d048ce85e
yanuarthasaputra5 <yanuarthasaputra5@gmail.com>
2026-06-21 22:18:30 +0700

    add: menambah seeder product

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -10,5 +10,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolePermissionSeeder::class);
         $this->call(StatusSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
